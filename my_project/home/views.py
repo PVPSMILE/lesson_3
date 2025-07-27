6754fad5c12dd5f69ab161f40bfd6047f64e1b14
Artem <artem@MacBook-Air-Artem.local>
2025-07-27 12:11:01 +0300

    db integration

--- my_project/home/views.py
+++ my_project/home/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import NavBar
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home/home.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        url = request.POST.get('url')
+        if name and url:
+            NavBar.objects.create(name=name, url=url)
+    nav_bar = NavBar.objects.all()
+    return render(request, 'home/home.html', {'nav_content': nav_bar})
